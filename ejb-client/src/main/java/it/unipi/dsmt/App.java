995c78a93118f9c9a0e1e5c807f617f1b40bd542
Tommy <62212917+Tomawk@users.noreply.github.com>
2022-01-20 17:15:01 +0100

    Modules fixed

--- ejb-client/src/main/java/it/unipi/dsmt/App.java
+++ ejb-client/src/main/java/it/unipi/dsmt/App.java
@@ -1,13 +1,11 @@
 package it.unipi.dsmt;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        UserRemote userRemote = null;
     }
 }
