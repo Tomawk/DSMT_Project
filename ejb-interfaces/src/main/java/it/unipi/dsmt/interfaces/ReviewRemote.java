61d458065a5606f4f33fa387a3b5e2494833a3db
Clarissa Polidori <73442844+clarisa1997@users.noreply.github.com>
2022-01-22 11:44:39 +0100

    gestione delle stelline per il rating medio

--- ejb-interfaces/src/main/java/it/unipi/dsmt/interfaces/ReviewRemote.java
+++ ejb-interfaces/src/main/java/it/unipi/dsmt/interfaces/ReviewRemote.java
@@ -1,7 +1,6 @@
 package it.unipi.dsmt.interfaces;
 
 import it.unipi.dsmt.dto.ReviewDTO;
-import it.unipi.dsmt.dto.UserDTO;
 
 import javax.ejb.Remote;
 import java.sql.SQLException;
@@ -9,8 +8,12 @@ import java.util.ArrayList;
 
 @Remote
 public interface ReviewRemote {
-    public ArrayList<ReviewDTO> getPetSitterReviewList(String petSitter) throws SQLException;
+
+    ArrayList<ReviewDTO> getPetSitterReviewList(String petSitter) throws SQLException;
+
     void insertReview(String owner, String pet_sitter, String text, int rating) throws SQLException;
 
     void deleteReview(int reviewId) throws SQLException;
+
+    float computeAvgRating(String petSitter) throws SQLException;
 }
