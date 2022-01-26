d234b16209c45d5855b883e02ba94cafaed08eb4
Tommy <62212917+Tomawk@users.noreply.github.com>
2022-01-26 19:21:35 +0100

    JPA Queries

--- ejb-impl/src/main/java/it/unipi/dsmt/ejb/ReviewRemoteEJB.java
+++ ejb-impl/src/main/java/it/unipi/dsmt/ejb/ReviewRemoteEJB.java
@@ -1,42 +1,53 @@
 package it.unipi.dsmt.ejb;
 
 import it.unipi.dsmt.dto.ReviewDTO;
+import it.unipi.dsmt.ejb.entities.Reviews;
+import it.unipi.dsmt.ejb.entities.Users;
 import it.unipi.dsmt.interfaces.ReviewRemote;
 
 import javax.ejb.Stateless;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 @Stateless
 public class ReviewRemoteEJB implements ReviewRemote {
 
     private static DataSource dataSource = null;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     public ReviewRemoteEJB() throws NamingException {
         Context ctx = new InitialContext();
         dataSource = (DataSource) ctx.lookup("jdbc/dsmt_project");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("dsmt_projectPU");
+        entityManager = emf.createEntityManager();
     }
 
-    @Override
     public ArrayList<ReviewDTO> getPetSitterReviewList(String petSitter) throws SQLException {
         ArrayList<ReviewDTO> returned_list = new ArrayList<ReviewDTO>();
-            Connection con = dataSource.getConnection();
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from review r inner join users u1 inner join users u2 where r.pet_sitter = " +
-                    "u1.user_id AND r.owner = u2.user_id AND u1.username='" + petSitter + "' "); //Retrieve all review about this pet sitter
-            while (rs.next()) {
-                ReviewDTO reviewDTO = new ReviewDTO(rs.getInt(1), rs.getString(2), rs.getString(21), rs.getString(3),
-                        rs.getString(4), rs.getInt(5), rs.getTimestamp(6));
-                returned_list.add(reviewDTO);
-            }
-            con.close();
-            return returned_list;
+        Users user = entityManager.createQuery(
+                "SELECT u from Users u WHERE u.username = :username", Users.class).
+                setParameter("username", petSitter).getSingleResult();
+        List<Reviews> reviews_list = entityManager.createQuery(
+                "SELECT u from Reviews u WHERE u.petsitter = :petsitter_id", Reviews.class).
+                setParameter("petsitter_id", user.getUser_id()).getResultList();
+        for(Reviews item:reviews_list){
+            Users owner = entityManager.find(Users.class,item.getOwner());
+            ReviewDTO reviewDTO = new ReviewDTO(item.getId(),item.getOwner(),owner.getUsername(),item.getPetsitter(),item.getText(),item.getRating(),item.getTimestamp());
+            returned_list.add(reviewDTO);
+        }
+        return returned_list;
     }
-
 
     @Override
     public void insertReview(String owner, String pet_sitter, String text, int rating) throws SQLException {
