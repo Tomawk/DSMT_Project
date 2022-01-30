03020dcf42e3d4e8eaa4203222f1eedaaa4fdaf3
matteoGuidotti <matteoguidotti98@gmail.com>
2022-01-30 16:41:56 +0100

    chat

--- erlang-server/start_chat/src/start_chat.erl
+++ erlang-server/start_chat/src/start_chat.erl
@@ -6,7 +6,7 @@
 
 
 start(_Type, _Args) ->
-  {ok, _ControllerPid} = id_handler_server_sup:start_link(),
+  {ok, _ControllerPid} = id_handler_server:start_link(),
   {ok, ServerPid} = message_server:start_link(),
   io:fwrite("Chat server started successfully!~n"),
   {ok, ServerPid}.
